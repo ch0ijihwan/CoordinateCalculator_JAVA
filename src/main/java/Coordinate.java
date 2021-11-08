55dd3465a203640912335f2977096a43cc6afc79
cjh8746 <cjh87467@gmail.com>
2021-11-08 18:51:59 +0900

    feat : Coordinate 구현

---
+++ src/main/java/Coordinate.java
@@ -0,0 +1,16 @@
+public class Coordinate {
+    private int x;
+    public int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y= y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+}
