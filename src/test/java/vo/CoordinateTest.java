17aac42129ea650de28fe8e71cba1c0556afe30f
cjh8746 <cjh87467@gmail.com>
2021-11-08 19:03:55 +0900

    test : Coordinate를 vo객체로 분류

--- src/test/java/vo/CoordinateTest.java
+++ src/test/java/vo/CoordinateTest.java
@@ -1,8 +1,9 @@
+package vo;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import vo.Coordinate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
