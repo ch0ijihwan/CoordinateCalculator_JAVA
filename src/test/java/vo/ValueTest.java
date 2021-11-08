f71fb438b4a5ea5dc4f13839be909055ebf22642
cjh8746 <cjh87467@gmail.com>
2021-11-08 18:11:54 +0900

    test : 테스트 코드 수정 실수 형태의 문자열을 입력 받았을 시, 예외처리 하도록 변환

--- src/test/java/vo/ValueTest.java
+++ src/test/java/vo/ValueTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValueTest {
-    private Value value;
-    
     @Test
     @DisplayName("Value의 값이 정수가 아닌경우 IllegalArgumentException 예외처리를 발생함.")
     void validateValue()
     {
         //give
-        double input = 0.1;
-
+        String input = "0.1";
 
         //then
         assertThatThrownBy(() -> new Value(input))
