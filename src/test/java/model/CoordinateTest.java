749560f164e5283d169982d7af5b5933f90174a3
cjh8746 <cjh87467@gmail.com>
2021-12-19 18:17:23 +0900

    test : 테스트 코드 추가

--- src/test/java/model/CoordinateTest.java
+++ src/test/java/model/CoordinateTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -27,5 +28,20 @@ class CoordinateTest {
         assertThatThrownBy(() -> new Coordinate(inputtedCoordinate))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("좌표의 값은 0 ~ 24 사이의 값 이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("getValue() 호출 시, 값을 반환한다.")
+    void getValue() {
+        //given
+        int inputtedValue = 1;
+        int expectedValue = 1;
+        Coordinate coordinate = new Coordinate(inputtedValue);
+
+        //when
+        int actual = coordinate.getValue();
+
+        //then
+        assertThat(actual).isEqualTo(expectedValue);
     }
 }
