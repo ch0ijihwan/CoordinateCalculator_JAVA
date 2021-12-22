de2a7969d75d0cc3df467b7de8aab7b84ab3ad86
cjh8746 <cjh87467@gmail.com>
2021-12-22 16:12:27 +0900

    refactor : 매직넘버 생성

--- src/main/java/view/ShowDisplay.java
+++ src/main/java/view/ShowDisplay.java
@@ -3,9 +3,9 @@ package view;
 public class ShowDisplay {
     private static final String INPUT_MESSAGE = "좌표를 입력해주세요.";
     private static final String INVALID_INPUT_ERROR_MESSAGE = "입력 값이 유효하지 않습니다";
-    private static final String LINE_PRINT_DISTANCE = "두 점 사이 거리는";
-    private static final String SHAPE_PRINT_AREA = "%s의 넓이는 %f%n";
-
+    private static final String LINE_PRINT_DISTANCE = "두 점 사이 거리는 :";
+    private static final String SHAPE_PRINT_AREA = "%s의 넓이는 : %f%n";
+    private static final String LINE = "직선";
     private ShowDisplay(){}
     public static void showInputDisplay() {
         System.out.println(INPUT_MESSAGE);
@@ -16,7 +16,7 @@ public class ShowDisplay {
     }
 
     public static void showArea(String shape, double area) {
-        if (shape.equals("직선")) {
+        if (shape.equals(LINE)) {
             System.out.println(LINE_PRINT_DISTANCE + area);
             return;
         }
