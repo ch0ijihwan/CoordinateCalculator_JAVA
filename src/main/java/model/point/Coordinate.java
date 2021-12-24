24660fbda7ccbed8e0ee2514a8617855f933ce04
cjh8746 <cjh87467@gmail.com>
2021-12-24 16:32:24 +0900

    refactor : 매직넘버 수정

--- src/main/java/model/point/Coordinate.java
+++ src/main/java/model/point/Coordinate.java
@@ -3,8 +3,8 @@ package model.point;
 import java.util.Objects;
 
 public class Coordinate {
-    private static final int MIN_BOUNDARY = 0;
-    private static final int MAX_BOUNDARY = 24;
+    private static final int COORDINATE_MIN_BOUNDARY = 0;
+    private static final int COORDINATE_MAX_BOUNDARY = 24;
     private static final String INVALID_COORDINATE_BOUNDARY_MESSAGE = "좌표의 값은 0 ~ 24 사이의 값 이어야 합니다.";
 
     private final int value;
@@ -15,7 +15,7 @@ public class Coordinate {
     }
 
     private void validateCoordinate(int value) {
-        if (value < MIN_BOUNDARY || value > MAX_BOUNDARY) {
+        if (value < COORDINATE_MIN_BOUNDARY || value > COORDINATE_MAX_BOUNDARY) {
             throw new IllegalArgumentException(INVALID_COORDINATE_BOUNDARY_MESSAGE);
         }
     }
